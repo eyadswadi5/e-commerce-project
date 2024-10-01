57a459d3331a69bc5557f6ee64bbf31a5d47f523
eyad al swadi <eyadswadi5@example.com>
2024-10-01 03:15:25 +0300

    create migration files

---
+++ database/migrations/2024_09_30_232202_create_payment_methods_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('payment_methods', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->foreignUuid('user_id')->constrained("users")->onDelete("cascade");
+            $table->string('type');
+            $table->json('details')->encrypted();
+            $table->boolean('is_primary')->default(false);
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('payment_methods');
+    }
+};
---
+++ database/migrations/2024_09_30_232202_create_reviews_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('reviews', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->foreignUuid('user_id')->constrained("users")->onDelete("cascade");
+            $table->foreignUuid('product_id')->constrained("products")->onDelete("cascade");
+            $table->integer('rating')->default(1)->min(1)->max(5);
+            $table->text('comment');
+            $table->boolean('approved')->default(false);
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('reviews');
+    }
+};
